ebd7a313590233fb649e6d84dfdf0d1e4fada2bd
AJinks28 <aekadam2002@gmail.com>
2024-08-17 19:44:09 +0530

    change layout

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        © 2024 WhiperBack. All rights reserved.
       </footer>
     </>
   );
